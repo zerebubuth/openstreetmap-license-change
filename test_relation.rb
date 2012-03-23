c64b33be3124fc7254b8d7220b1a6267f6230973
Richard Fairhurst <richard@systemeD.net>
2012-03-23 12:28:18 +0000

    Add a (currently failing) relation test

--- test_relation.rb
+++ test_relation.rb
@@ -24,4 +24,19 @@ class TestRelation < Test::Unit::TestCase
                   Redact[OSM::Relation, 1, 1, :hidden]
                  ], actions)
   end
+  
+  # relation created by decliner, then members added by agreer.
+  # Under the v0 principle, we can keep the new members, but not the v1 members or the tags
+  def test_relation_members_added
+    history = [OSM::Relation[[ [OSM::Way,1] ,               [OSM::Way,2]               ], :id => 1,  :changeset => 3,  :version => 1, "type" => "route" ],
+               OSM::Relation[[ [OSM::Way,1] , [OSM::Way,4], [OSM::Way,2]               ], :id => 1,  :changeset => 2,  :version => 2, "type" => "route" ],
+               OSM::Relation[[ [OSM::Way,1] , [OSM::Way,4], [OSM::Way,2], [OSM::Way,3] ], :id => 1,  :changeset => 1,  :version => 3, "type" => "route" ]]
+    bot = ChangeBot.new(@db)
+    actions = bot.action_for(history)
+    assert_equal([Edit[OSM::Relation[[ [OSM::Way,4], [OSM::Way,3] ], :id => 1,  :changeset => -1,  :version => 3]],
+                  Redact[OSM::Relation, 1, 1, :hidden],
+                  Redact[OSM::Relation, 1, 2, :visible],
+                  Redact[OSM::Relation, 1, 3, :visible]
+                 ], actions)
+  end
 end
