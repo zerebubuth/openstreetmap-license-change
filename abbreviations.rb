1c411bbe83af5311b3b4568945a5539d2f512992
Michael Bemmerl <mail@mx-server.de>
2012-05-03 00:52:16 +0200

    Added german abbreviations.

--- abbreviations.rb
+++ abbreviations.rb
@@ -155,6 +155,14 @@ module Abbrev
     "посёлок" => ["поселок", "пос"],
     "деревня" => ["дер", "д"],
     "квартал" => ["кв-л", "кв"],
+
+    # German abbreviations
+    "straße" => ["str."],
+    "platz" => ["pl."],
+    "gasse" => ["g."],
+    "weg" => ["wg."],
+    # Swiss German
+    "strasse" => ["str."],
   }
 
   # of course, this is horribly english-specific...
