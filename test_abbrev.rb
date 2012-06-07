0617be3a9de120b5228917817fc55a03ed90b943
Gnonthgol <gnonthgol@gmail.com>
2012-06-07 17:46:02 +0200

    New test setup for abbreviations

--- test_abbrev.rb
+++ test_abbrev.rb
@@ -12,60 +12,58 @@ require 'minitest/unit'
 
 class TestAbbrev < MiniTest::Unit::TestCase
 
-  # test that some strings are the same under abbreviation, 
-  # even when there's differing whitespace or punctuation
-  # involved.
-  def test_abbrev_english
-    check_abbrev_equality("Foobar Rd", "Foobar Road")
-    check_abbrev_equality("Foobar Road", "Foobar Rd")
-    check_abbrev_equality("E Foobar Wy", "East Foobar Way")
-    check_abbrev_equality("NE Foobar Crescent", "North East Foobar Cr.")
-    check_abbrev_equality("N.E. Foobar Crescent", "North East Foobar Cr")
-    check_abbrev_equality("NE Foobar Crescent", "Northeast Foobar Cr")
-    check_abbrev_equality("Foo & Bar", "Foo and Bar")
-    check_abbrev_equality("Foo&Bar", "Foo and Bar")
-    check_abbrev_equality("North & Western", "North and Western")
+  EQUAL_STRINGS = {
+    # English
+    "Foobar Rd" => "Foobar Road",
+    "Foobar Road" => "Foobar Rd",
+    "E Foobar Wy" => "East Foobar Way",
+    "NE Foobar Crescent" => "North East Foobar Cr.",
+    "N.E. Foobar Crescent" => "North East Foobar Cr",
+    "NE Foobar Street" => "Northeast Foobar St",
+    "Foo & Bar" => "Foo and Bar",
+    "Foo&Bar" => "Foo and Bar",
+    "North & Western" => "North and Western",
+    # Russian
+    "бул. Космонавтов" => "бульвар Космонавтов",
+    "пр-кт. Надеяться" => "проспект Надеяться",
+    # German
+    "Joh.-Seb.-Bach-Straße" => "Johann-Sebastian-Bach-str.",
+    "Bettina-v-Arnim-Straße" => "Bettina-von-Arnim-Straße",
+    "Universität Münster" => "Uni Münster",
+    "Kindergarten Jahnstraße" => "KiGa Jahnstraße",
+    "Kl. Moor" => "Kleines Moor",
+    "Realschule Stralsund" => "RS Stralsund",
+    "Bgm. Willhelm str." => "Bürgermeister Willhelm straße",
+    "Hubertus-Platz" => "Hubertus-Pl.",
+    "Fachhochschule Bremen" => "FH Bremen",
+    "An der Bahn" => "A. d. Bahn",
+    "Groß Ippener" => "Gr. Ippener",
+    "Klein Ippener" => "Kl Ippener",
     
-    #some INequality tests. check ampersands are not getting swallowed.
-    check_abbrev_inquality("& & A & B &&", "A & B")
-    check_abbrev_inquality("Foo & Bar", "Foo Bar")
+    "Streitwagenwg" => "Streitwagenweg",
+    "Musterwg." => "Musterweg",
+    "Herreng." => "Herrengasse",
+    "Hauptstrasse" => "Hauptstr.",
+    "Hauptstr." => "Hauptstraße",
+    "Nürnbergerstraße" => "Nürnberger Str.",
+  }
+  
+  INQUAL_STRINGS = {
+    # English
+    "& & A & B &&" => "A & B",
+    "Foo & Bar" => "Foo Bar",
+  }
+
+  EQUAL_STRINGS.each do |k, v|
+    define_method("test_abbrev_#{k}") {
+      check_abbrev_equality("#{k}", "#{v}")
+    }
   end
   
-  def test_abbrev_russian
-    check_abbrev_equality("бул. Космонавтов", "бульвар Космонавтов")
-      
-    # This fails because we split on punctuation including dash
-    # Fails immediately because of word count comparison, but wouldn't work either way
-    check_abbrev_equality("пр-кт. Надеяться", "проспект Надеяться")
-  end
-  
-  def test_abbrev_german_normal
-    check_abbrev_equality("Joh.-Seb.-Bach-Straße", "Johann-Sebastian-Bach-str.")
-    check_abbrev_equality("Bettina-v-Arnim-Straße","Bettina-von-Arnim-Straße")
-    check_abbrev_equality("Universität Münster","Uni Münster")
-    check_abbrev_equality("Kindergarten Jahnstraße","KiGa Jahnstraße")
-    check_abbrev_equality("Kl. Moor","Kleines Moor")
-    check_abbrev_equality("Realschule Stralsund","RS Stralsund")
-    check_abbrev_equality("Bgm. Willhelm str.", "Bürgermeister Willhelm straße")
-    check_abbrev_equality("Hubertus-Platz", "Hubertus-Pl.")
-    check_abbrev_equality("Fachhochschule Bremen","FH Bremen")
-    check_abbrev_equality("An der Bahn","A. d. Bahn")
-    check_abbrev_equality("Groß Ippener","Gr. Ippener")
-    check_abbrev_equality("Klein Ippener","Kl Ippener")
-  end
-    
-  def test_abbrev_german_abbrev_word_end
-    # These fail because the abbreviated portion is part of a larger word (at the end)
-    check_abbrev_equality("Streitwagenwg","Streitwagenweg")
-    check_abbrev_equality("Musterwg.", "Musterweg")
-    check_abbrev_equality("Herreng.", "Herrengasse")
-    check_abbrev_equality("Hauptstrasse", "Hauptstr.")
-    check_abbrev_equality("Hauptstr.", "Hauptstraße")
-  end
-    
-  def test_abbrev_german_diff_wordcounts
-    # This fails because the different versions have different word counts
-    check_abbrev_equality("Nürnbergerstraße","Nürnberger Str.")
+  INQUAL_STRINGS.each do |k, v|
+    define_method("test_abbrev_#{k}") {
+      check_abbrev_inquality("#{k}", "#{v}")
+    }
   end
 
   private
