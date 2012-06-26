02d9208eac16a6d531cfe85606cc4a060c1e88fb
Andy Allan <andy@gravitystorm.co.uk>
2012-06-26 15:42:47 +0100

    Fetch region from tracker, split into smaller areas, and build list of entities via map calls and candidate filtering.

--- run_bot.rb
+++ run_bot.rb
@@ -9,6 +9,7 @@ require 'pg'
 require 'getoptlong'
 require 'oauth'
 require 'yaml'
+require 'xml/libxml'
 
 def usage
   puts <<-EOF
@@ -42,12 +43,49 @@ def print_time(verbose, name = nil)
   print "#{name}..." if verbose and not name.nil?
 end
 
+def get_next_region()
+  # for now just grab next region from list
+  # TODO don't grab regions adjacent to in-progress regions
+  # TODO ensure transaction so that parrallel instances don't both grab same region
+  res = @tracker_conn.exec("select id, lat, lon from regions where status = 'unprocessed' order by id limit 1")
+  @region = {id: res[0]['id'], lat: res[0]['lat'].to_f, lon: res[0]['lon'].to_f}
+  @tracker_conn.exec("update regions set status = 'processing' where id = $1", [@region[:id]])
+  @region
+end
+
+def size_of_area(a)
+  (a[:maxlat] - a[:minlat]) * (a[:maxlon] - a[:minlon])
+end
+
+# split an area into two areas, and add them to the list
+# divide along the longest edge
+def split_area(a, list)
+  a1 = a.clone
+  a2 = a.clone
+  lat_range = a[:maxlat] - a[:minlat]
+  lon_range = a[:maxlon] - a[:minlon]
+  if lat_range > lon_range
+    a1[:maxlat] = a[:minlat] + lat_range/2
+    a2[:minlat] = a[:minlat] + lat_range/2
+  else
+    a1[:maxlon] = a[:minlon] + lon_range/2
+    a2[:minlon] = a[:minlon] + lon_range/2
+  end
+
+  list.push(a1).push(a2)
+end
+
 verbose = false
 no_action = false
+
+MAX_REQUEST_AREA = 0.25
 
 auth = YAML.load(File.open('auth.yaml'))
 oauth = auth['oauth']
 dbauth = auth['database']
+trackerauth = auth['tracker']
+
+@tracker_conn = PGconn.open( :dbname => trackerauth['dbname'] )
 
 opts.each do |opt, arg|
   case opt
@@ -67,11 +105,79 @@ if not ARGV.empty?
   exit 0
 end
 
+@nodes = []
+@ways = []
+@relations = []
+
+@candidate_nodes = []
+@candidate_ways = []
+@candidate_relations = []
+
+res = @tracker_conn.exec("select osm_id from candidates where type = 'node' and status = 'unprocessed'")
+res.each do |r|
+  @candidate_nodes << r['osm_id'].to_i
+end
+
+res = @tracker_conn.exec("select osm_id from candidates where type = 'way' and status = 'unprocessed'")
+res.each do |r|
+  @candidate_ways << r['osm_id'].to_i
+end
+
+res = @tracker_conn.exec("select osm_id from candidates where type = 'relation' and status = 'unprocessed'")
+res.each do |r|
+  @candidate_relations << r['osm_id'].to_i
+end
+
+
 print_time(verbose, 'Connecting to the database')
-PGconn.open( :host => dbauth['host'], :port => dbauth['port'], :dbname => dbauth['dbname'], :user => dbauth['user'], :password => dbauth['password'] ).transaction do |dbconn|
+PGconn.open( :host => dbauth['host'], :port => dbauth['port'], :dbname => dbauth['dbname'] ).transaction do |dbconn|
   db = PG_DB.new(dbconn)
   bot = ChangeBot.new(db)
-  
+
+  region = get_next_region()
+
+  areas = [{minlat: region[:lat], maxlat: (region[:lat] + 1), minlon: region[:lon], maxlon: (region[:lon] + 1)}]
+
+  while areas.length > 0
+    puts "#{areas.length} areas remaining" if verbose
+    area = areas.pop
+    if size_of_area(area) > MAX_REQUEST_AREA
+      split_area(area, areas)
+      next
+    else
+      puts "processing #{area}" if verbose
+      map = Net::HTTP.get_response(URI(oauth['site'] + "/api/0.6/map?bbox=#{area[:minlon]},#{area[:minlat]},#{area[:maxlon]},#{area[:maxlat]}"))
+      case map.code
+      when '509'
+        puts "Darn, rate throttled!"
+        # TODO handle this properly
+        exit(1)
+      when '400' # too many entities
+        split_area(area, areas)
+        next
+      when '200'
+        parser = XML::Reader.string(map.body)
+        while parser.read
+          next unless ["node", "way", "relation"].include? parser.name
+          id = parser['id'].to_i
+          case parser.name
+          when "node"
+            @nodes << id if @candidate_nodes.include? id
+          when "way"
+            @ways << id if @candidate_ways.include? id
+          when "relation"
+            @relations << id if @candidate_relations.include? id
+          end
+        end
+      else
+        puts "Unhandled response code #{map.code}"
+        exit(1)
+      end
+    end
+  end
+
+  db.set_entities({node: @nodes, way: @ways, relation: @relations})
+
   print_time(verbose, 'Processing all nodes')
   bot.process_nodes!
   print_time(verbose, 'Processing all ways')
