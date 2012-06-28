0c797c1a85a39aca820704b8410c178bfbeb960c
Andy Allan <andy@gravitystorm.co.uk>
2012-06-28 17:44:16 +0100

    Fix up get_auth so that it doesn't clobber the whole file.

--- get_auth.rb
+++ get_auth.rb
@@ -10,26 +10,27 @@ require 'rubygems'
 require 'oauth'
 require 'yaml'
 
+y = YAML.load(File.open('auth.yaml'))
+
 # Format of auth.yml:
 # consumer_key: (from osm.org)
 # consumer_secret: (from osm.org)
 # token: (use oauth setup flow to get this)
 # token_secret: (use oauth setup flow to get this)
-auth={}
 
 puts "First, go register a new application at "
-puts "http://api06.dev.openstreetmap.org/oauth_clients/new"
+puts y["oauth"]["site"]
 puts "Tick the appropriate boxes"
 puts "Enter the consumer key you are assigned:"
-auth["consumer_key"] = gets.strip
+y["oauth"]["consumer_key"] = gets.strip
 puts "Enter the consumer secret you are assigned:"
-auth["consumer_secret"] = gets.strip
+y["oauth"]["consumer_secret"] = gets.strip
 puts "Your application is now set up, but you need to register"
 puts "this instance of it with your user account."
 
-@consumer=OAuth::Consumer.new auth["consumer_key"],
-                              auth["consumer_secret"],
-                              {:site=>"http://localhost:3000"}
+@consumer=OAuth::Consumer.new y["oauth"]["consumer_key"],
+                              y["oauth"]["consumer_secret"],
+                              {:site=>y["oauth"]["site"]}
 
 @request_token = @consumer.get_request_token
 
@@ -40,9 +41,9 @@ verifier = gets.strip
 puts "Converting request token into access token..."
 @access_token=@request_token.get_access_token(:oauth_verifier => verifier)
 
-auth["token"] = @access_token.token
-auth["token_secret"] = @access_token.secret
+y["oauth"]["token"] = @access_token.token
+y["oauth"]["token_secret"] = @access_token.secret
 
-File.open('auth.yaml', 'w') {|f| YAML.dump(auth, f)}
+File.open('auth.yaml', 'w') {|f| YAML.dump(y, f)}
 
 puts "Done. Have a look at auth.yaml to see what's there."
