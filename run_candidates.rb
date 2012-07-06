dc9b96ce2dc9422db7844cce2c845416e3a27737
Andy Allan <andy@gravitystorm.co.uk>
2012-07-06 18:37:22 +0100

    Calculate a lat/lon for each candidate (except silly nested relations)

--- run_candidates.rb
+++ run_candidates.rb
@@ -11,6 +11,8 @@ require 'xml/libxml'
 # are defined elsewhere - this candidate list is only an optimisation, not a decision-making
 # step. Running this with "select osm_id as id from nodes;" should have no impact on results.
 
+SCALE = 10000000
+
 auth = YAML.load(File.open('auth.yaml'))
 
 tracker_auth = auth['tracker']
@@ -29,7 +31,7 @@ if res.num_tuples == 0
   @tracker_conn.exec("CREATE TYPE entity_type  as enum ('node', 'way', 'relation')")
 end
 
-@tracker_conn.exec("create table if not exists candidates (type entity_type, osm_id bigint, status candidate_status default 'unprocessed')")
+@tracker_conn.exec("create table if not exists candidates (type entity_type, osm_id bigint, status candidate_status default 'unprocessed', lat real, lon real)")
 @tracker_conn.exec("truncate table candidates")
 
 parser = XML::Reader.io(File.open('additional_users.xml', "r"))
@@ -40,6 +42,34 @@ while parser.read
   @additional_uids << parser['uid']
 end
 
+def lat_lon_for_entity(type, id)
+  lat = nil
+  lon = nil
+  case type
+  when 'node'
+    r = @dbconn.query(NODE_LOCATION_SQL % {:id => id})
+    if r.num_tuples > 0
+      lat, lon = r[0]['latitude'].to_i / SCALE, r[0]['longitude'].to_i / SCALE
+    end
+  when 'way'
+    r = @dbconn.query(WAY_LOCATION_SQL % {:id => id})
+    if r.num_tuples > 0
+      lat, lon = r[0]['latitude'].to_i / SCALE, r[0]['longitude'].to_i / SCALE
+    end
+  when 'relation'
+    r = @dbconn.query(RELATION_LOCATION_BY_NODE % {:id => id})
+    if r.num_tuples > 0
+      lat, lon = r[0]['latitude'].to_i / SCALE, r[0]['longitude'].to_i / SCALE
+    else
+      puts "2"
+      r2 = @dbconn.query(RELATION_LOCATION_BY_WAY % {:id => id})
+      if r2.num_tuples > 0
+        lat, lon = r2[0]['latitude'].to_i / SCALE, r2[0]['longitude'].to_i / SCALE
+      end
+    end
+  end
+  return lat, lon
+end
 
 EACH_SQL = \
   'SELECT %{type}s.%{type}_id AS id
@@ -50,13 +80,43 @@ EACH_SQL = \
     OR users.id in (%{uid_list})
     GROUP BY %{type}s.%{type}_id;'
 
+NODE_LOCATION_SQL = \
+    'SELECT latitude, longitude FROM current_nodes
+    WHERE id = %{id}'
+
+WAY_LOCATION_SQL = \
+    'SELECT latitude, longitude
+      FROM way_nodes
+      JOIN nodes on way_nodes.node_id = nodes.node_id
+      WHERE way_id = %{id}
+      LIMIT 1'
+
+RELATION_LOCATION_BY_NODE = \
+      "SELECT latitude, longitude
+       FROM relation_members
+       JOIN nodes on relation_members.member_id = nodes.node_id
+       WHERE relation_members.member_type = 'Node'
+       AND relation_id = %{id}
+       LIMIT 1"
+
+RELATION_LOCATION_BY_WAY = \
+      "SELECT latitude, longitude
+        FROM relation_members
+        JOIN way_nodes on relation_members.member_id = way_nodes.way_id
+        JOIN nodes on way_nodes.node_id = nodes.node_id
+        WHERE relation_members.member_type = 'Way'
+        AND relation_id = %{id}
+        LIMIT 1"
+
 out = @tracker_conn.transaction do |tracker_conn|
   tracker_conn.exec("copy candidates FROM STDIN WITH csv")
   ['node', 'way', 'relation'].each do |type|
     res = @dbconn.query(EACH_SQL % {:type => type, :uid_list => @additional_uids.join(",")})
     puts "#{res.num_tuples} #{type}s"
     res.each do |r|
-      line = "#{type},#{r['id']},unprocessed\n"
+      id = r['id']
+      lat, lon = lat_lon_for_entity(type, id)
+      line = "#{type},#{id},unprocessed,#{lat},#{lon}\n"
       tracker_conn.put_copy_data(line)
     end
   end
