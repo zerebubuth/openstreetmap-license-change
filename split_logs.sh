1b6f0028573ebc4fbc0f51eecd5181a229a862b6
Gnonthgol <gnonthgol@gmail.com>
2012-07-18 01:43:50 +0200

    Add script to split logs by pid.
    Thank to Ilpo Jarvinen for the script.
    NOTE: Make sure that manual additions to the log files have the correct pid before this script is run.

---
+++ split_logs.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+LOG_DIR="logs"
+LOG_FILES=`ls -d $LOG_DIR/* | grep -v -e "-"`
+
+for file in $LOG_FILES
+do
+  echo "Splitting file $file"
+
+  awk '
+  {
+
+    fname=gensub(".log$","","h",FILENAME);
+    if (prev != fname) {
+      header=""; sep=""; inhdr=1;
+    }
+    prev=fname;
+
+    if (substr($3, 1, 1) == "#") {
+      inhdr=0;
+      pid=gensub("[]#]","","g",$3);
+      if (!(pid in started)) {
+        print header > fname "-" pid ".log"
+        started[pid]=1;
+      }
+      print $0 > fname "-" pid ".log"
+    } else {
+      if (inhdr) {
+        header=header sep $0;
+        sep="\n";
+      } else {
+        print $0 > fname "-" pid ".log"
+      }
+  }}' $file
+done
+
+#echo removing old log files
+#rm $LOG_FILES
